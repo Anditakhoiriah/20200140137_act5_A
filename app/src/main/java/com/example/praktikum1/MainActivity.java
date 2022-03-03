0ed136db6a613e9abb3641cb3794fdb63d7603a7
AnditaKhoiriah <andita.khoiriah.ft20@mail.umy.ac.id>
2022-03-03 11:58:27 +0700

    commit 5

--- app/src/main/java/com/example/praktikum1/MainActivity.java
+++ app/src/main/java/com/example/praktikum1/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.praktikum1;
 
 import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Deklarasi variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edEmail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Menghubungkan variabel btnLogin dengan componen button pada layout
+        btnLogin=findViewById(R.id.btSignin);
+
+        //Menghubungkan variabel edEmail dengan componen button pada layout
+        edEmail=findViewById(R.id.edEmail);
+
+        //Menghubungkan variabel edPassword dengan componen button pada layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //membuat fungsi one klik pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Menyimpan input user di edittext email kedalam variabel nama
+                nama = edEmail.getText().toString();
+
+                //Menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+                //membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "email anda: " + nama + " dan Password anda: " + password + "", Toast.LENGTH_LONG);
+
+                //memvalidasi inputan user
+                if (nama.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Login Gagal", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Sukses", Toast.LENGTH_SHORT).show();
+                }
+
+                //Menampilkan toast
+                t.show();
+
+            }
+        });
+
     }
 }
